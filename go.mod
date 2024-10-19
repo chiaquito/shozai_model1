60d841741d745f886c338a3daa83cec21bf46260
chiaquito <chiaki.aws@gmail.com>
2024-10-20 03:55:49 +0900

    read env variable for dbconf

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module shozai_model1
 go 1.23.1
 
 require (
+	github.com/caarlos0/env/v11 v11.2.2
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-sql-driver/mysql v1.8.1
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.12.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
@@ -15,7 +15,6 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -28,5 +27,5 @@ require (
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"time"
 
+	env "github.com/caarlos0/env/v11"
 	"github.com/go-sql-driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,26 +15,11 @@ type config struct {
 }
 
 func New() *config {
-	jst, err := time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		// エラーハンドリング
-	}
-	dbConf := &mysql.Config{
-		DBName: "db",
-		User:   "root",
-		Passwd: "password",
-		// Addr:      "localhost:3306",
-		Addr:      "db:3306",
-		Net:       "tcp",
-		ParseTime: true,
-		Collation: "utf8mb4_unicode_ci",
-		Loc:       jst,
-		Params:    map[string]string{
-			// "usessl": "false",
-			// "allowPublicKeyRetrieval": "true",
-		},
-	}
 
+	dbConf, err := newDBConf()
+	if err != nil {
+		fmt.Println(err)
+	}
 	gormConf := &gorm.Config{
 		CreateBatchSize: 1000,
 	}
@@ -41,4 +28,36 @@ func New() *config {
 		DB:   dbConf,
 		Gorm: gormConf,
 	}
+}
+
+type dbConf struct {
+	Host     string `env:"DB_HOST" envDefault:"db"`
+	Port     string `env:"DM_PORT" envDefault:"3306"`
+	Name     string `env:"DB_NAME" envDefault:"db"`
+	User     string `env:"DB_USER" envDefault:"root"`
+	PassWord string `env:"DB_PASSWORD" envDefault:"password"`
+}
+
+func newDBConf() (*mysql.Config, error) {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var dbCfg dbConf
+	if err := env.Parse(&dbCfg); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return &mysql.Config{
+		DBName:    dbCfg.Name,
+		User:      dbCfg.User,
+		Passwd:    dbCfg.PassWord,
+		Addr:      fmt.Sprintf("%s:%s", dbCfg.Host, dbCfg.Port),
+		Net:       "tcp",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}, nil
 }
