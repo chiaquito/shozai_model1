1a4869102f18c86707951c30c2dcb066acecd271
chiaquito <chiaki.aws@gmail.com>
2025-01-05 05:50:43 +0900

    modify ip

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 	e.Validator = &Validator{validator: validator.New()}
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:1323", "http://54.248.124.112:1323", "http://10.0.1.74:1323"},
+		AllowOrigins: []string{"http://localhost:1323", "http://35.72.231.58:1323", "http://10.0.1.105:1323"},
 	}))
 
 	e.Logger.Fatal(e.Start(":1323"))
